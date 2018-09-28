e658b05acf97f3023406784a0dbfa4a0ffb27a40
EvgeneOskin <eoskin@crystalnix.com>
2018-09-28 15:15:17 +0600

    Fix synchronization for person calls

--- src/client.js
+++ src/client.js
@@ -1,34 +1,36 @@
 const messages = require('./protos/hello_pb');
 const services = require('./protos/hello_grpc_pb');
 const grpc = require('grpc');
-const { promisify } = require('util');
+const async = require('async');
 
 const client = new services.HelloClient('localhost:50051', grpc.credentials.createInsecure());
 
+function makeName(first, last) {
+  const name = new messages.Name()
+  name.setFirst(first)
+  name.setLast(last)
+  return name
+}
 
-const me = new messages.Name()
-me.setFirst('Eugene')
-me.setLast('Oskin')
-promisify(client.world)(me)
-  .then(console.log)
-  .catch(console.error)
+const me = makeName('Eugene', 'Oskin')
+
+client.world(me, (err, welcome) =>
+  err ? console.error(err) : console.log(welcome.getOutput())
+)
 
 const names = [
-  new messages.Name({ first: 'Gabriel', last: 'Márquez' }),
-  new messages.Name({ first: 'Martin', last: 'Fowler' })
+  makeName('Gabriel', 'Márquez'),
+  makeName('Martin', 'Fowler' ),
 ]
 
 function callNames(callback) {
   const call = client.person();
 
-  call.on('data', welcome => console.log(welcome.output));
+  call.on('data', welcome => console.log(welcome.getOutput()));
   call.on('end', callback)
 
   names.forEach(name => call.write(name));
   call.end();
 }
-/*
-promisify(callNames)()
-  .then(console.log)
-  .catch(console.error)
-  */
+
+callNames(() => ({}))
--- src/server.js
+++ src/server.js
@@ -1,29 +1,41 @@
 const grpc = require('grpc');
-
+const _ = require('lodash');
 const messages = require('./protos/hello_pb');
 const services = require('./protos/hello_grpc_pb');
+
 
 function world(call, callback) {
   const first = call.request.getFirst()
   const last = call.request.getLast()
 
+
   const welcome = new messages.Welcome()
   welcome.setOutput(`Hello ${first} ${last}. I'm World!`)
 
-  callback(null, welcome);
+  _.delay(() => callback(null, welcome), 2000);
 }
 
-function person(call, callback) {
-  call.on('data', (name) => {
-    const first = name.getFirst()
-    const last = name.getLast()
+function person(call) {
+  let number = 1
+  const names = []
 
-    const welcome = new messages.Welcome()
-    welcome.setOutput(`Hello ${first} ${last}. I'm World!`)
+  call.on('data', (name) => names.push(name))
+  call.on('end', () => {
+    names.forEach((name, i) => {
+      const first = name.getFirst()
+      const last = name.getLast()
 
-    call.write(welcome)
+      const welcome = new messages.Welcome()
+      welcome.setOutput(`Hello ${first} ${last}. I'm World!`)
+
+      _.delay(() => {
+        call.write(welcome)
+        if (i >= names.length - 1) {
+          call.end()
+        }
+      }, 2000 * number);
+    });
   });
-  call.on('end', () => call.end());
 }
 
 function serve() {
